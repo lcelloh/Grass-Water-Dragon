8d9d91e610046cf37806d916a52cecb0ddb18d7f
lcelloh <cellittilorenzoh@gmail.com>
2022-02-10 18:14:43 +0100

    rewrote functions and logic, finally works!!

--- index.js
+++ index.js
@@ -1,74 +1,77 @@
 function computerPlay() {
-  let selectedNum = Math.floor(Math.random() * 3);
-
-  if (selectedNum === 0) {
-    return "rock";
-  } else if (selectedNum === 1) {
-    return "paper";
-  } else if (selectedNum === 2) {
-    return "scissor";
-  }
+  const choice = ["rock", "paper", "scissor"];
+  let sign = choice[Math.floor(Math.random() * 3)];
+  return sign;
 }
 
 function playerSelection() {
-  return prompt("What do you want to play? ", "cancel");
+  let choice = prompt("What do you want to play? ", "rock");
+  
+
+  return choice;
 }
 
-const playerChoice = playerSelection();
-const computerChoice = computerPlay();
-console.log(computerChoice);
-console.log(playerChoice);
+function game() {
+  let flag = true,
+    playerPoints = 0,
+    computerPoints = 0;
 
-// console.log(playRound(playerChoice, computerChoice));
-const round = playRound();
-let playerPoints = new Array();
-let computerPoints = new Array();
-
-/*function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-    if (playerPoints.length === 3) {
-      return "Player has won!";
-      break;
-    } else if (computerPoints.length === 3) {
-      return "Computer has won!";
-      break;
-    } else if (round === "Tie") {
-      //pass
-    } else if (round === "Computer wins") {
-      computerPoints.push(i);
-    } else if (round === "Player wins") {
-        playerPoints.push(i);
+  while (flag) {
+    let manche = round();
+    if (manche === "PlayerWin") {
+      console.log("Player won the round");
+      playerPoints += 1;
+    } else if (manche === "ComputerWin") {
+      console.log("Computer won the round");
+      computerPoints += 1;
+    } else if (manche === "tie") {
     }
+    console.log(`Player score: ${playerPoints}`);
+    console.log(`Computer score: ${computerPoints}`);
+    if (computerPoints === 3 || playerPoints === 3) {
+      alert("game over");
+      flag = false;
     }
-}
-console.log(game())*/
-
-function playRound(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return "Tie";
-  } else if (playerChoice === null || playerChoice === "cancel") {
-    return "Canceled";
-  } else if (playerChoice === "rock") {
-    if (computerChoice === "paper") {
-      return "Computer wins";
-    } else if (computerChoice === "scissor") {
-      return "Player Wins";
-    }
-  } else if (playerChoice === "paper") {
-    if (computerChoice === "scissor") {
-      return "Computer wins";
-    } else if (computerChoice === "rock") {
-      return "Player Wins";
-    }
-  } else if (playerChoice === "scissor") {
-    if (computerChoice === "rock") {
-      return "Computer wins";
-    } else if (computerChoice === "paper") {
-      return "Player Wins";
-    }
+  }
+  if (playerPoints > computerPoints) {
+    console.log("Player Wins !");
+  } else {
+    console.log("Computer Wins");
   }
 }
 
-
-
+function round() {
+  let play = computerPlay(),
+    selection = playerSelection();
+  console.log(`Player choosed: ${selection}`);
+  console.log(`Computer choosed: ${play}`);
+  if (play === selection) {
+    console.log("it's a tie");
+    return "tie";
+  } else if (selection === "paper" && play !== "paper") {
+    if (play === "rock") {
+      console.log(`Paper beats rock`);
+      return "PlayerWin";
+    } else if (play === "scissor") {
+      console.log(`Scissors beat paper`);
+      return "ComputerWin";
+    }
+  } else if (selection === "rock" && play !== "rock") {
+    if (play === "paper") {
+      console.log(`Paper beats rock`);
+      return "ComputerWin";
+    } else if (play === "scissor") {
+      console.log(`Rock beats scissor`);
+      return "PlayerWin";
+    }
+  } else if (selection === "scissor" && play !== "scissor") {
+    if (play === "paper") {
+      console.log(`Scissor beats paper`);
+      return "PlayerWin";
+    } else if (play === "rock") {
+      console.log(`Rock beats scissor`);
+      return "ComputerWin";
+    }
+  }
+}
+game();
