70094164da2130d846aca69409864a7cc0afe609
lcelloh <cellittilorenzoh@gmail.com>
2022-02-17 18:58:50 +0100

    almost finished ui

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 function game() {
   let flag = true,
     playerPoints = 0,
@@ -35,43 +37,43 @@ function round() {
   if (play === selection) {
     console.log("it's a tie");
     return "tie";
-  } else if (selection === "paper" && play !== "paper") {
-    if (play === "rock") {
-      console.log(`Paper beats rock`);
+  } else if (selection === "dragon" && play !== "dragon") {
+    if (play === "grass") {
+      console.log(`dragon beats grass`);
       return "PlayerWin";
-    } else if (play === "scissor") {
-      console.log(`Scissors beat paper`);
+    } else if (play === "water") {
+      console.log(`waters beat dragon`);
       return "ComputerWin";
     }
-  } else if (selection === "rock" && play !== "rock") {
-    if (play === "paper") {
-      console.log(`Paper beats rock`);
+  } else if (selection === "grass" && play !== "grass") {
+    if (play === "dragon") {
+      console.log(`dragon beats grass`);
       return "ComputerWin";
-    } else if (play === "scissor") {
-      console.log(`Rock beats scissor`);
+    } else if (play === "water") {
+      console.log(`grass beats water`);
       return "PlayerWin";
     }
-  } else if (selection === "scissor" && play !== "scissor") {
-    if (play === "paper") {
-      console.log(`Scissor beats paper`);
+  } else if (selection === "water" && play !== "water") {
+    if (play === "dragon") {
+      console.log(`water beats dragon`);
       return "PlayerWin";
-    } else if (play === "rock") {
-      console.log(`Rock beats scissor`);
+    } else if (play === "grass") {
+      console.log(`grass beats water`);
       return "ComputerWin";
     }
   }
 }
 function computerPlay() {
-  const choice = ["rock", "paper", "scissor"];
+  const choice = ["grass", "dragon", "water"];
   let sign = choice[Math.floor(Math.random() * 3)];
   return sign;
 }
 
 function playerSelection() {
-  let choice = prompt("What do you want to play? ", "rock");
+  let choice = prompt("What do you want to play? ", "grass");
 
   return choice;
 }
 
-//console.log("Rock-Paper-Scissors game, bo3");
+console.log("grass-dragon-waters game, bo3");
 //game();
